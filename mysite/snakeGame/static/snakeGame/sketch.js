c17dc9f12edb824076b2233e3bdccf8ec3e787f2
Vitor Azevedo Padovani <98499539+Vitor-Padovani@users.noreply.github.com>
2022-03-21 11:20:21 -0300

    food_spawn

--- mysite/snakeGame/static/snakeGame/sketch.js
+++ mysite/snakeGame/static/snakeGame/sketch.js
@@ -1,24 +1,37 @@
-playerColors = new colors();
-player1 = new snake();
 let scale = 20;
+let fpsBaseValue = 10;
+let fps;
 
 function preload() {
 
 }
 
 function setup() {
+  playerColors = new colors();
+  player1 = new snake();
+  food = new food();
+
   url_info();
 
-  createCanvas(400, 400);
-  frameRate(30);
+  createCanvas(640, 640);
+  frameRate(fpsBaseValue);
+
+  food.pick_location();
+}
+
+function mousePressed() {
+  food.pick_location();
 }
 
 function draw() {
   background(127);
+
   fill(playerColors.red, playerColors.green, playerColors.blue);
-  ellipse(mouseX, mouseY, 100);
   player1.update();
   player1.show();
+
+  fill(255, 0, 0);
+  rect(food.vector.x, food.vector.y, scale);
 }
 
 function url_info() {
@@ -32,6 +45,18 @@ function url_info() {
     playerColors.blue = color.slice(6, 9);
   } catch {
     console.log('Color Error;')
+  }
+}
+
+function key_pressed(player) {
+  if (((keyCode == UP_ARROW) | (keyCode == 87)) & (player.y_speed < 1)) {
+    player.dir(0, -1);
+  } else if (((keyCode == RIGHT_ARROW) | (keyCode == 68)) & (player.x_speed > -1)) {
+    player.dir(1, 0);
+  } else if (((keyCode == DOWN_ARROW) | (keyCode == 83)) & (player.y_speed > -1)) {
+    player.dir(0, 1);
+  } else if (((keyCode == LEFT_ARROW) | (keyCode == 65)) & (player.x_speed < 1)) {
+    player.dir(-1, 0);
   }
 }
 
@@ -56,6 +81,8 @@ function snake() {
   this.tail = 0;
 
   this.update = function() {
+    key_pressed(this);
+
     this.x = this.x + this.x_speed*scale;
     this.y = this.y + this.y_speed*scale;
 
@@ -67,5 +94,21 @@ function snake() {
       rect(this.tail[i].x, this.tail[i].y, scale);
     }
     rect(this.x, this.y, scale);
+  }
+  this.dir = function(x, y) {
+    this.x_speed = x;
+    this.y_speed = y;
+  }
+}
+
+function food() {
+  this.vector = createVector();
+
+  this.pick_location = function() {
+    var cols = floor(width/scale);
+    var rows = floor(height/scale);
+
+    this.vector = createVector(floor(random(1, cols-1)), floor(random(1, rows-1)));
+    this.vector.mult(scale);
   }
 }
